84cccb24ec38dc70d101f5002eeab995edaa7a54
Faraz <farazpourebrahimh@gmail.com>
2024-08-13 12:14:20 +0330

    customized fetcher (connect.ts) implemented and added

--- SET_internship/src/App.tsx
+++ SET_internship/src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
-
+import Connect from "./api/connect";
+import BASE_URL from "./api/endpoints";
 import React from "react";
 
 function App() {
-
+    React.useEffect(() => {
+        const con: Connect = new Connect(BASE_URL);
+        async function func() {
+            const data = await con.post("api/v1/auth/user/validate",{mobile: "09141035058"});
+        }
+        func();
+    }, []);
 
   return (
     <>
